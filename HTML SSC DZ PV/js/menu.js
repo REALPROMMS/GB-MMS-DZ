a1bdb384a687e3c541f1a530991ea71fb45ec211
Mihail <m.makarov90@bk.ru>
2021-08-16 17:34:28 +0300

    Сделал корявый адаптив главной страницы :)

--- HTML SSC DZ PV/js/menu.js
+++ HTML SSC DZ PV/js/menu.js
@@ -1,10 +1,11 @@
 const menuBtn = document.querySelector('.btn-menu');
 const wrap = document.querySelector('.menu__list');
 const closeBtn = document.querySelector('.btn-menu__close');
-menuBtn.addEventListener('click', () => {
-	wrap.classList.toggle('menu__opened');
-});
 
-closeBtn.addEventListener('click', () => {
+
+let toggleClass = function() {
 	wrap.classList.toggle('menu__opened');
-});
+} 
+
+menuBtn.addEventListener('click', toggleClass);
+closeBtn.addEventListener('click', toggleClass);
